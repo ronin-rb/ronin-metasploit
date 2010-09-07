7c9fb5e58e44584012432ca59da7e92f1bd8ddec
Postmodern <postmodern.mod3@gmail.com>
2010-09-06 22:39:12 -0700

    Added missing requirements.

--- lib/ronin/metasploit/note.rb
+++ lib/ronin/metasploit/note.rb
@@ -18,7 +18,9 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
+require 'ronin/metasploit/workspace'
 require 'ronin/metasploit/host'
+require 'ronin/metasploit/service'
 require 'ronin/metasploit/model'
 
 module Ronin
