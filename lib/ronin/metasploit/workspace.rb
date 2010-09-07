fb479abc0a14121ecdefd56bcd4ffb291d635404
Postmodern <postmodern.mod3@gmail.com>
2010-09-06 21:47:43 -0700

    Added Ronin::Metasploit::Workspace.

---
+++ lib/ronin/metasploit/workspace.rb
@@ -0,0 +1,45 @@
+#
+# Ronin Metasploit - A Ronin library for accessing The Metasploit Framework.
+#
+# Copyright (c) 2010 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+require 'ronin/metasploit/model'
+
+module Ronin
+  module Metasploit
+    class Workspace
+
+      include Model
+
+      storage_names[:metasploit] = 'workspaces'
+
+      property :id, Serial
+
+      property :name, String, :length => 255
+
+      property :created_at, DateTime
+
+      property :updated_at, DateTime
+
+      property :boundary, Text, :length => 4096
+
+      property :description, Text, :length => 4096
+
+    end
+  end
+end
